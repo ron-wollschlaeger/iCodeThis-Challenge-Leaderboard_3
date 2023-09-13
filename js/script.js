0d0ff04a5c161bbdecbe53dc0f793c086bc2a078
ron-wollschlaeger <ron.wollschlaeger@frontshape.de>
2023-09-13 18:45:43 +0200

    Upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+        extend: {
+            fontFamily: {
+                header: ['Roboto Condensed'],
+                body: ['Roboto']
+            }
+        },
+    },
+    plugins: [],
+}
